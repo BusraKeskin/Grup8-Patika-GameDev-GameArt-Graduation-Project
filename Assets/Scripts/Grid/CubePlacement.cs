1d3abf3029a18d787f4bae314dfb810d6a768a30
Selin Morali <selinmoralii@gmail.com>
2022-08-04 21:14:33 +0300

    Grid system created.

---
+++ Assets/Scripts/Grid/CubePlacement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlacement : MonoBehaviour
+{
+    public GameObject ObjectToMove;
+    public GameObject ObjectToPlace;
+    public LayerMask Mask;
+    public float LastPositionY;
+    public Vector3 MousePosition;
+    private Renderer Render;
+    public Material GridMaterial, DefaultMaterial;
+
+    void Start()
+    {
+        Render = GameObject.Find("Grid").GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        MousePosition = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(MousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, Mask))
+        {
+            int PosX = (int)Mathf.Round(hit.point.x);
+            int PosZ = (int)Mathf.Round(hit.point.z);
+
+            ObjectToMove.transform.position = new Vector3(PosX, LastPositionY, PosZ);
+            Render.material = GridMaterial;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(ObjectToPlace, ObjectToMove.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Render.material = DefaultMaterial;
+        }
+    }
+}
---
+++ Assets/Scripts/Grid/DragAndDrop.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject ObjectToPlace;
+    public LayerMask Mask;
+    public float LastPositionY;
+    public Vector3 MousePosition;
+    private Renderer Render;
+    public Material GridMaterial, DefaultMaterial;
+    private bool _isDraging;
+
+
+    void Start()
+    {
+        Render = GameObject.Find("Grid").GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _isDraging = true;
+            MousePosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(MousePosition);
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, Mask))
+            {
+                int PosX = (int)Mathf.Round(hit.point.x);
+                int PosZ = (int)Mathf.Round(hit.point.z);
+
+                ObjectToPlace.transform.position = new Vector3(PosX, LastPositionY, PosZ);
+            }
+        }
+        else
+        {
+            _isDraging = false;
+        }
+
+
+        if (_isDraging)
+        {
+            Render.material = GridMaterial;
+        }
+        else if (!_isDraging)
+        {
+            Render.material = DefaultMaterial;
+        }
+    }
+}
