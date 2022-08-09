a92357991bad1f71ce4c511db582fad8307e1aaa
Selin Morali <selinmoralii@gmail.com>
2022-08-09 22:34:09 +0300

    Changes made to UI Manager script.

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,17 +4,63 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public enum UIStates { Main, Settings, CardHero }; //UI States
+    public UIStates CurrentState;
+    public GameObject MainPanel;
+    public GameObject SettingsPanel; //Ayarlar butonuna t?kland???nda aç?lacak panel
+    public GameObject HeroCardPanel; //Hero card butonuna t?kland???nda aç?lacak panel
 
-    void Start()
+    private void Awake()
     {
-
+        CurrentState = UIStates.Main;
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        // UI durumlar?n?n kontrolü
+        switch (CurrentState)
         {
-            EventManager.loadSameScene?.Invoke(); //Event calistirma yontemi
+            case UIStates.Main:
+                MainPanel.SetActive(true);
+                SettingsPanel.SetActive(false);
+                HeroCardPanel.SetActive(false);
+                break;
+            case UIStates.Settings:
+                SettingsPanel.SetActive(true);
+                MainPanel.SetActive(true);
+                HeroCardPanel.SetActive(false);
+                break;
+            case UIStates.CardHero:
+                HeroCardPanel.SetActive(true);
+                MainPanel.SetActive(true);
+                SettingsPanel.SetActive(false);
+                break;
         }
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.R))
+    //    {
+    //        EventManager.loadSameScene?.Invoke(); //Event calistirma yontemi
+    //    }
+    //}
+
+
+    public void OnMainMenu()
+    {
+        CurrentState = UIStates.Main;
+    }
+
+    public void OnSettings()
+    {
+        //Settings butonununa t?klan?ld???nda
+        CurrentState = UIStates.Settings;
+    }
+
+    public void OnCardHero()
+    {
+        //Hero card butonuna t?kland???nda
+        CurrentState = UIStates.CardHero;
     }
 }
